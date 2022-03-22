f1efa35c1e30005c25d4357a368d6110fb37c83a
Alex Geis <siegxela@gmail.com>
2022-03-22 11:23:33 -0600

    added user-pass-val.js file example and cookie template info to server.js

--- server.js
+++ server.js
@@ -3,19 +3,29 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
-const sess = {
-  secret: "Super secret secret",
-  resave: false,
-  saveUninitialized: false,
-};
-app.use(session(sess));
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {
+//     // Stored in milliseconds (86400 === 1 day)
+//     maxAge: 86400,
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
+// app.use(session(sess));
+app.use(session(process.env.sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
