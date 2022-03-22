648bb061ed29c6b336b826a8b6992e3886bf545b
Alex Geis <siegxela@gmail.com>
2022-03-22 11:11:32 -0600

    added express session boilerplate examples to server.js and ex-routes.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: "Super secret secret",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -16,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./controllers"));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
