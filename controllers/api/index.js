7ffe8517174c67ddda5b6e2e14aed48f13756392
Alex Geis <siegxela@gmail.com>
2022-03-24 13:56:56 -0600

    updated main.handlebars JS link, removed ex-routes.js, fixed server connection issues in server.js, updated / and /login API get routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 
 //EXAMPLE NEW ROUTE
-const exRoutes = require("./ex-routes");
 const userExRoutes = require("./user-ex-routes");
-router.use("/ex", exRoutes);
 router.use("/users", userExRoutes);
 
 module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const { User, Event } = require("../models/");
+
+// get all posts
+router.get("/", async (req, res) => {
+  try {
+    // const postData = await Post.findAll({
+    //   include: [User],
+    // });
+
+    // const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.json("this will become the home page soon... we promise...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one post
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render("single-post", { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get login redirect
+router.get("/login", (req, res) => {
+  //   if (req.session.loggedIn) {
+  //     res.redirect("/dashboard");
+  //     return;
+  //   }
+
+  res.render("login");
+});
+
+// //get signup redirect
+// router.get("/signup", (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect("/dashboard");
+//     return;
+//   }
+
+//   res.render("login");
+// });
+
+module.exports = router;
